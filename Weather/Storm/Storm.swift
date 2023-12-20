01fc21b11a7524ae793a7616f59b89952029737c
peterSanyo <peter.sanyo@code.berlin>
2023-12-20 12:06:49 +0100

    + Storm: +  update function: improve motion logic

--- Weather/Storm/Storm.swift
+++ Weather/Storm/Storm.swift
@@ -30,8 +30,33 @@ class Storm {
     }
     
     func update(date: Date, size: CGSize) {
+        // ensuring movement is smooth over time regardless of frame Rate
         let delta = date.timeIntervalSince1970 - lastUpdate.timeIntervalSince1970
         
+        for drop in drops {
+            
+            let radians = drop.direction.radians // making it possible to trigonometrical calculate new position based on direction (Angle -> radians)
+            
+            // Update the x position of the drop based on its speed, direction, and time delta.
+            drop.x += cos(radians) * drop.speed * delta // cos(radians) gives the horizontal component of the direction.
+            drop.y += sin(radians) * drop.speed * delta // sin(radians) for the vertical component.
+
+            // Horizontal wrapping
+            if drop.x < -0.2 {
+                drop.x += 1.4
+            }
+
+            // Vertical Wrapping
+            if drop.y > 1.2 {
+                drop.x = Double.random(in: -0.2...1.2)
+                drop.y -= 1.4
+            }
+            
+            // update rotation to give a realistic tumble movement
+            drop.rotation += drop.rotationSpeed * delta
+        }
+        
+        // Store the current date as the last update time to calculate the delta in the next update cycle, ensuring consistent and smooth animations.
         lastUpdate = date
     }
 }
