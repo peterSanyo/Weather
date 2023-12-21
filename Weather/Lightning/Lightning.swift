ede360af7099b3d7e9713a7b0771cea2b36fc35f
peterSanyo <peter.sanyo@code.berlin>
2023-12-21 10:42:27 +0100

    + random and organic forking of lightning

--- Weather/Lightning/Lightning.swift
+++ Weather/Lightning/Lightning.swift
@@ -32,13 +32,20 @@ class Lightning {
                 guard var lastPoint = bolt.points.last else { continue }
 
                 for _ in 0..<5 {
-                    let endX = lastPoint.x + (speed * cos(bolt.angle))
-                    let endY = lastPoint.y - (speed * sin(bolt.angle))
+                    let endX = lastPoint.x + (speed * cos(bolt.angle) + Double.random(in: -10...10))
+                    let endY = lastPoint.y - (speed * sin(bolt.angle) + Double.random(in: -10...10))
                     lastPoint = CGPoint(x: endX, y: endY)
                     bolt.points.append(lastPoint)
                 }
+                
                 if lastPoint.y < size.height {
                     hasFinishedStriking = false
+                    
+                    if bolts.count < 4 && Int.random(in: 0..<100) < 20 {
+                        let newAngle = Double.random(in: -.pi / 4 ... .pi / 4) - .pi / 2
+                        let newBolt = LightningBolt(start: lastPoint, width: bolt.width * 0.35, angle: newAngle)
+                        bolts.append(newBolt)
+                    }
                 }
             }
             if hasFinishedStriking {
@@ -54,6 +61,7 @@ class Lightning {
     
     func strike() {
         guard bolts.isEmpty else { return }
+        state = .striking
         
         let startPosition = CGPoint(x: 200, y:0)
         let newBolt = LightningBolt(start: startPosition, width: 5, angle: Angle.degrees(270).radians)
